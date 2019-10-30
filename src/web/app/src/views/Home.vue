276b92c9b09eed4f398340b5c4cac34b15060a5f
DanKhoo <danielkhoo94@hotmail.com>
2019-10-30 17:20:34 +0800

    Home.vue: Remove unnecessary information in home page

--- src/web/app/src/views/Home.vue
+++ src/web/app/src/views/Home.vue
@@ -2,14 +2,8 @@
   <div>
     <div>
       <el-container>
-        <el-aside>
-          <ListOfGuide/>
-        </el-aside>
         <el-main>
           <WelcomeMessage/>
-          <FeatureGuide/>
-          <br/>
-          <DetailedFeatureGuide/>
         </el-main>
       </el-container>
     </div>
@@ -19,26 +13,16 @@
 <script>
   // @ is an alias to /src
   import WelcomeMessage from '@/components/homePageDetail/WelcomeMessage.vue'
-  import FeatureGuide from '@/components/homePageDetail/FeatureGuide.vue'
-  import DetailedFeatureGuide from '@/components/homePageDetail/DetailedFeatureGuide.vue'
-  import ListOfGuide from '@/components/homePageDetail/ListOfGuide.vue'
 
   export default {
     name: 'home',
     components: {
       WelcomeMessage,
-      FeatureGuide,
-      DetailedFeatureGuide,
-      ListOfGuide
     }
   }
 </script>
 <style scoped>
-  .el-aside {
-    width: 250px;
-  }
-
   .el-main {
-    margin: 30px;
+    margin: 50px 200px;
   }
 </style>
